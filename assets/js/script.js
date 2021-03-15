fb698ac5a3de77e173ff0c499550459c47c3da34
ArlPortofolio <79692638+ArlPortofolio@users.noreply.github.com>
2021-03-15 23:31:06 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,18 @@ window.addEventListener("scroll", function () {
   var header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
 
-  var popup = document.getElementById("popup");
-  popup.classList.toggle("show", window.scrollY > 1300);
+  const menuBTn = this.document.querySelector(".menu-btn");
+  let menuOpen = false;
+  menuBTn.addEventListener("click", () => {
+    if (!menuOpen) {
+      menuBTn.classList.add("open");
+      menuOpen = true;
+    } else {
+      menuBTn.classList.remove("open");
+      menuOpen = false;
+    }
+
+    var popup = document.getElementById("popup");
+    popup.classList.toggle("show", window.scrollY > 1300);
+  });
 });
