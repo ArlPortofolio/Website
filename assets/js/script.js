f277f4152c86da39ea03abb2449b3620a8eccad0
ArlPortofolio <79692638+ArlPortofolio@users.noreply.github.com>
2021-03-22 20:38:56 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,28 @@
-window.addEventListener("scroll", function () {
-  var header = document.querySelector("header");
-  header.classList.toggle("sticky", window.scrollY > 0);
+window.addEventListener('scroll', function () {
+	var header = document.querySelector('header');
+	header.classList.toggle('sticky', window.scrollY > 0);
+});
 
-  const menuBTn = this.document.querySelector(".menu-btn");
-  let menuOpen = false;
-  menuBTn.addEventListener("click", () => {
-    if (!menuOpen) {
-      menuBTn.classList.add("open");
-      menuOpen = true;
-    } else {
-      menuBTn.classList.remove("open");
-      menuOpen = false;
-    }
+window.addEventListener('scroll', function () {
+	var popup = document.getElementById('popup');
+	popup.classList.toggle('show', window.scrollY > 1300 + 200);
+});
 
-    var popup = document.getElementById("popup");
-    popup.classList.toggle("show", window.scrollY > 1300);
-  });
+const menuBTn = this.document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBTn.addEventListener('click', () => {
+	if (!menuOpen) {
+		menuBTn.classList.add('open');
+		menuOpen = true;
+	} else {
+		menuBTn.classList.remove('open');
+		menuOpen = false;
+	}
+});
+
+$('.item-header').click(function () {
+	$('.accordion-item').removeClass('active');
+	$(this).parent().addClass('active');
+	$('.icon').text('+');
+	$(this).children('.icon').text('-');
 });
